ae8b0fd40e01a5b68248d92ed7a4e6c20bfec946
nomakewan <nomakewan@users.noreply.github.com>
2026-07-26 15:40:22 -0700

    Recompile binaries

--- source/StreamEffects/include/BuildInfo.h
+++ source/StreamEffects/include/BuildInfo.h
@@ -1,7 +1,3 @@
 #pragma once
 // Reflect the last build date for this binary.
-<<<<<<< HEAD
-const char* build_date = "V6_20260726172833";
-=======
-const char* build_date = "V6_20260726142649";
->>>>>>> 622bb646117c41c251c3250cfa8114d774e2bdb0
+const char* build_date = "V6_20260726153243";
